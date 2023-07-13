70c862d005d120d1c2742858a92709279339a576
lenkon <inoulenkon@gmail.com>
2023-07-13 18:25:04 +0530

    Add aggregate queries

--- queries.sql
+++ queries.sql
@@ -45,3 +45,16 @@ ROLLBACK TO savePointUpdate;
 UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
 COMMIT;
 SELECT * FROM animals;
+
+/* Project 2 - Agregate queries */
+SELECT COUNT(*) FROM animals;
+
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg) FROM animals;
+
+SELECT neutered, MAX(escape_attempts) FROM animals GROUP BY neutered;
+
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
