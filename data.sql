b4325cd63562bb3b2265c1d67c46393e49b586cf
lenkon <inoulenkon@gmail.com>
2023-07-14 09:08:07 +0530

    Insert given data into owners and species tables

--- data.sql
+++ data.sql
@@ -23,3 +23,14 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Blossom', '1998-10-13', 3, true, 17);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Ditto', '2022-05-14', 4, true, 22);
+
+/* Project 3 - Populate owners and species tables and modify animals table */
+INSERT INTO owners(full_name,age) VALUES
+('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
+
+INSERT INTO species(name) VALUES ('Pokemon'), ('Digimon');
